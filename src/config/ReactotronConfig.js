a6e3ef1971dd9b7176b767a8e66eff9f97d84218
William França Macedo <william@localhost.localdomain>
2019-09-23 22:13:48 -0300

    Update host config to Reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,10 @@
 import Reactotron from 'reactotron-react-native';
 
+const ip_casa = '192.168.15.7';
+const ip_trabalho = '10.68.101.90';
+
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '10.68.101.90' })
+  const tron = Reactotron.configure({ host: ip_casa })
     .useReactNative()
     .connect();
 
